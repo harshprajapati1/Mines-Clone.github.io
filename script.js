10a85d2e0d62bf58e93a48f26bd03bdd64a7eec2
Harsh Prajapati <harsh.prajapati7890@gmail.com>
2024-06-03 00:38:35 +0530

    added the setup panel and box for game

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-// let box = document.createElement("div")
-// box.id='b1'
+// // let box = document.createElement("div")
+// // box.id='b1'
 
-// const game = document.getElementsByClassName('game')
+// // const game = document.getElementsByClassName('game')
 
-// box.style.backgroundColor = red;
+// // box.style.backgroundColor = red;
 
-function addElement() {
-    for(let i=0;i<25;i++){
-    const k=5;
-    let rows = []
-    if(i%k === 0){
-        rows.push(document.createElement("div"))
-        rows[rows.length-1].id = "row-no-"+rows.length.toString();
-        const currentDiv = document.getElementById("game");
-        currentDiv.appendChild(newDiv)
-    }
+// function addElement() {
+//     for(let i=0;i<25;i++){
+//     const k=5;
+//     let rows = []
+//     if(i%k === 0){
+//         rows.push(document.createElement("div"))
+//         rows[rows.length-1].id = "row-no-"+rows.length.toString();
+//         document.getElementById("row-no-0").textContent = "hehe";
+//         const currentDiv = document.getElementById("script");
+//         currentDiv.insertBefore('script')
+//     }
     
-    const newDiv = document.createElement('div')
-    newDiv.id=i;
-    rows[rows.length-1].appendChild(newDiv);
-    }
-  }
+//     // const newDiv = document.createElement('div')
+//     // newDiv.id=i;
+//     // rows[rows.length-1].appendChild(newDiv);
+//     }
+//   }
 
-  addElement()
+//   addElement()
