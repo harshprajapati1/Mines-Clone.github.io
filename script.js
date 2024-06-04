86a0e88f607c79fa1e61d520f20fd57c5481e5e6
Harsh Prajapati <harsh.prajapati7890@gmail.com>
2024-06-04 14:01:39 +0530

    added the game block

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
-// // let box = document.createElement("div")
-// // box.id='b1'
+// import math
+function addElement(){
+   
+    for(let i=0;i<25;i++){
+        let newDiv = document.createElement("div");
+        newDiv.id = i;
+        let val = Math.ceil((i+1)/5);
+        let blockName = "block" + val;
+        newDiv.className = blockName;
+        const game = document.getElementById("game");
+        game.appendChild(newDiv);
+    }
+  }
 
-// // const game = document.getElementsByClassName('game')
-
-// // box.style.backgroundColor = red;
-
-// function addElement() {
-//     for(let i=0;i<25;i++){
-//     const k=5;
-//     let rows = []
-//     if(i%k === 0){
-//         rows.push(document.createElement("div"))
-//         rows[rows.length-1].id = "row-no-"+rows.length.toString();
-//         document.getElementById("row-no-0").textContent = "hehe";
-//         const currentDiv = document.getElementById("script");
-//         currentDiv.insertBefore('script')
-//     }
-    
-//     // const newDiv = document.createElement('div')
-//     // newDiv.id=i;
-//     // rows[rows.length-1].appendChild(newDiv);
-//     }
-//   }
-
-//   addElement()
+  addElement()
