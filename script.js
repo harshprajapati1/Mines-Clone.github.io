025cb9d5b4c4eae0a7c204455f15237293cce53c
Harsh Prajapati <harsh.prajapati7890@gmail.com>
2024-06-04 16:38:06 +0530

    added javascript for basic 3 mine game

--- script.js
+++ script.js
@@ -1,15 +1,53 @@
-// import math
-function addElement(){
-   
-    for(let i=0;i<25;i++){
-        let newDiv = document.createElement("div");
-        newDiv.id = i;
-        let val = Math.ceil((i+1)/5);
-        let blockName = "block" + val;
-        newDiv.className = blockName;
-        const game = document.getElementById("game");
-        game.appendChild(newDiv);
+function initialize(){
+    let arr = []
+    while(arr.length!==3){
+        let num = Math.ceil((Math.random()*25))%25;
+        
+        if (!arr.includes(num))   {
+            arr.push(num);
+        }    
     }
-  }
+    return arr;
+}
+const arr = initialize()
+var mineCount = 0;
+const cons = document.getElementById('console');
 
-  addElement()
+function reset(){
+   
+    for(let i=1;i<=5;i++){
+    var parent = document.getElementById('block' + i);
+    var children = parent.children;
+    console.log(children);
+    
+    // Loop through the children and log each one
+    for (var j = 0; j < children.length; j++) {
+        console.log(children[j].style.backgroundColor = "#2F4452");
+    }
+}
+mineCount = 0;
+cons.innerHTML ="Mine counter: 0 (3 mine game)" ;
+}
+
+
+function call(el){
+ var blockNum = parseInt(el.className,10);
+console.log(blockNum)
+if(arr.includes(blockNum)){
+    el.style.backgroundColor = 'red';
+    mineCount++;
+    cons.innerHTML ="Mine Count: " + mineCount;
+}
+else{
+    el.style.backgroundColor = 'green';
+}
+if(mineCount>2){
+    cons.innerHTML ="Game Over" ;
+    setTimeout(reset, 2000); 
+}
+
+}
+function bet(){
+ console.log("bet button clicked");
+}
+
